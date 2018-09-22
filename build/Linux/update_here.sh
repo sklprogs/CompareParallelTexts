8b20bdaee2cc823af514d6a2c79658f6f1797559
skl.progs <skl.progs@gmail.com>
2018-09-22 12:37:51 +0300

    Build scripts

--- build/Linux/update_here.sh
+++ build/Linux/update_here.sh
@@ -1,22 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 # Do not use "verbose" in order to spot errors easily
 
 mkdir -p ./resources/locale/ru/LC_MESSAGES/
 
 # Copy shared resources
-cp -u /usr/local/bin/shared/resources/{error,info,question,warning,icon_64x64_cpt}.gif ./resources/
+cp -u $HOME/bin/shared/resources/{error,info,question,warning,icon_64x64_cpt}.gif ./resources/
 
 # Copy other CompareParallelTexts resources
-cp -u /usr/local/bin/CompareParallelTexts/resources/locale/ru/LC_MESSAGES/CompareParallelTexts.mo ./resources/locale/ru/LC_MESSAGES/
+cp -u $HOME/bin/CompareParallelTexts/resources/locale/ru/LC_MESSAGES/CompareParallelTexts.mo ./resources/locale/ru/LC_MESSAGES/
 
 # Copy CompareParallelTexts Python files
-cp -u /usr/local/bin/CompareParallelTexts/src/CompareParallelTexts.py .
+cp -u $HOME/bin/CompareParallelTexts/src/CompareParallelTexts.py .
 
 # Copy shared Python files
-cp -u /usr/local/bin/shared/src/{gettext_windows,shared,sharedGUI}.py .
+cp -u $HOME/bin/shared/src/{gettext_windows,shared,sharedGUI}.py .
 
 # (Linux-only) Copy build scripts
-cp -u /usr/local/bin/CompareParallelTexts/build/Linux/{build.sh,clean_up.sh,setup.py} .
+cp -u $HOME/bin/CompareParallelTexts/build/Linux/{build.sh,clean_up.sh,setup.py} .
 
 ls .
--- build/Wine/update_here.sh
+++ build/Wine/update_here.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 # Do not use "verbose" in order to spot errors easily
 
 mkdir -p ./resources/locale/ru/LC_MESSAGES/
 
 # Copy shared resources
-cp -u /usr/local/bin/shared/resources/{error,info,question,warning,icon_64x64_cpt}.gif ./resources/
+cp -u $HOME/bin/shared/resources/{error,info,question,warning,icon_64x64_cpt}.gif ./resources/
 
 # Copy other CompareParallelTexts resources
-cp -u /usr/local/bin/CompareParallelTexts/resources/locale/ru/LC_MESSAGES/CompareParallelTexts.mo ./resources/locale/ru/LC_MESSAGES/
+cp -u $HOME/bin/CompareParallelTexts/resources/locale/ru/LC_MESSAGES/CompareParallelTexts.mo ./resources/locale/ru/LC_MESSAGES/
 
 # Copy CompareParallelTexts Python files
-cp -u /usr/local/bin/CompareParallelTexts/src/CompareParallelTexts.py .
+cp -u $HOME/bin/CompareParallelTexts/src/CompareParallelTexts.py .
 
 # Copy shared Python files
-cp -u /usr/local/bin/shared/src/{gettext_windows,shared,sharedGUI}.py .
+cp -u $HOME/bin/shared/src/{gettext_windows,shared,sharedGUI}.py .
 
 # (Wine-only) Copy CompareParallelTexts icon
 cp -ru /home/pete/bin/CompareParallelTexts/resources/icon_64x64_cpt.ico ./resources/
 
 # (Wine-only) Copy build scripts
-cp -u /usr/local/bin/CompareParallelTexts/build/Wine/{build.sh,clean_up.sh,CompareParallelTexts.cmd,setup.py,update_cpt.sh} .
+cp -u $HOME/bin/CompareParallelTexts/build/Wine/{build.sh,clean_up.sh,CompareParallelTexts.cmd,setup.py,update_cpt.sh} .
 
 ls .
